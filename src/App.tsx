9b223760c25139ed66d6aa6d4db205cd5411aea6
Fred Haus <fredhaus@Freds-MacBook-Pro-235.local>
2021-03-11 10:59:11 +0100

    adds custom calendar interface

--- src/App.tsx
+++ src/App.tsx
@@ -3,6 +3,7 @@ import CalendarInterface from "./components/CalendarInterface";
 import InputEmployees from "./components/InputEmployees";
 import InputPositions from "./components/InputPositions";
 import InputShifts from "./components/InputShifts";
+import ManualCalendar from "./components/ManualCalendar";
 
 const App: FC = () => {
   const test = { test: "moin" };
@@ -10,7 +11,8 @@ const App: FC = () => {
   return (
     <div className="App">
       <h1>MyDienstplan</h1>
-      <CalendarInterface/>
+      {/* <CalendarInterface/> */}
+      <ManualCalendar/>
       <InputPositions/>
       <InputShifts {...test} />
       <InputEmployees />
--- src/components/CalendarInterface.tsx
+++ src/components/CalendarInterface.tsx
@@ -1,29 +1,43 @@
-import React, {useState} from 'react'
-import Calendar, {MonthView} from 'react-calendar';
-
+import React, { useState } from "react";
+import Calendar, { MonthView } from "react-calendar";
 
 function CalendarInterface() {
-
   const [value, setValue] = useState<any>(new Date());
 
-  console.log("value", value)
+  const ReturnDay = (dayNr: number) => {
+    switch (dayNr) {
+      case 6:
+        return "Saturday";
+      case 0:
+        return "Sunday";
+
+      default:
+        break;
+    }
+  };
+
+  console.log("value", value);
 
   return (
     <div>
-    <Calendar
-      onChange={setValue}
-      value={value}
-      defaultView="year"
-      onClickMonth={(value: any) => console.log("value______",value)}   
+      <Calendar
+        onChange={setValue}
 
-    />
-    <MonthView
+        value={value}
+        defaultView="year"
+        onClickMonth={(value: any) => console.log("value______MONTH", value)}
+        onClickDay={(value: any) => console.log("value______DAY", value)}
+        tileContent={({ date, view }) =>
+          view === "month" && ReturnDay(date.getDay()) ? (
+            <p style={{ backgroundColor: "red" }}>WE !</p>
+          ) : null
+        } // 0 = Sun, 6 = Sat
+      />
+      {/* <MonthView
       activeStartDate={new Date(2021, 1, 1)} // 1.2.2021 wegen Index 0
-    />
-  </div>
-  )
+    /> */}
+    </div>
+  );
 }
 
-
-
-export default CalendarInterface
+export default CalendarInterface;
